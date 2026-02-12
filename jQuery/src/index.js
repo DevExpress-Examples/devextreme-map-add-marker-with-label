88180aacf6761da223cd99dc303c0aaaf1953f7d
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-02-12 10:35:05 +0200

    Load index.js as module in jQuery
    (cherry picked from commit a3997a79ceb3d609779864b89f8ea357c38fafa9)

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,52 +1,12 @@
-function initMap() {
-  const map = $('#map').dxMap({
-    center: '49.47805, -123.84716',
-    zoom: 14,
-    width: 1200,
-    height: 620,
-    provider: 'google',
-    apiKey: {
-      google: 'YOUR_API_KEY',
-    },
-    providerConfig: {
-      mapId: 'DEMO_MAP_ID',
-    },
-    onReady(s) {
-      const googleMap = s.originalMap;
-
-      const createMarkerContent = (price, opacity = 1) => {
-        const content = document.createElement('div');
-        content.className = 'labels';
-        content.textContent = price;
-        content.style.opacity = opacity;
-        return content;
-      };
-
-      const marker1 = new google.maps.marker.AdvancedMarkerElement({
-        map: googleMap,
-        position: { lat: 49.47805, lng: -123.84716 },
-        content: createMarkerContent('$425K', 0.75),
-        // eslint-disable-next-line spellcheck/spell-checker
-        gmpDraggable: false,
-        // eslint-disable-next-line spellcheck/spell-checker
-        gmpClickable: false,
-      });
-
-      const marker2 = new google.maps.marker.AdvancedMarkerElement({
-        map: googleMap,
-        position: { lat: 49.475, lng: -123.84 },
-        content: createMarkerContent('$395K', 1.0),
-        // eslint-disable-next-line spellcheck/spell-checker
-        gmpDraggable: true,
-      });
-    },
-  }).dxMap('instance');
-}
-
-window.initMap = initMap;
+import { count as initialCount } from "./data.js";
 
 $(() => {
-  if (window.google && window.google.maps) {
-    initMap();
-  }
+  let count = initialCount;
+  $('#btn').dxButton({
+    text: `Click count: ${count}`,
+    onClick(e) {
+      count += 1;
+      e.component.option('text', `Click count: ${count}`);
+    },
+  });
 });
